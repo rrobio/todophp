94b4371ee3059bd8734c5e4a79cbd04aa978bcab
rrobio <orlandinirobi@gmail.com>
2023-10-18 09:26:23 +0200

    add togglestatus helper

--- index.php
+++ index.php
@@ -45,12 +45,31 @@ function removeTodo(int $id, SQLite3 $db): bool
     $result = $stmt->execute();
     return is_object($result);
 }
-//
-//function toggleStatus(int $id, string $status, SQLite3 $db): bool
-//{
-//    $stmt = $db->prepare('SELECT * FROM todo WHERE id=:id;');
-//    $stmt->bindValue(':id', $id, SQLITE3_INTEGER);
-//    $result = $stmt->execute();
+function toggleStatus(int $id, string $status, SQLite3 $db): bool
+{
+    $stmt = $db->prepare('SELECT * FROM todo WHERE id=:id;');
+    $stmt->bindValue(':id', $id, SQLITE3_INTEGER);
+    $result = $stmt->execute();
+
+    if (is_object($result)) {
+        $row = $result->fetchArray();
+        $state = (bool)$row[$status];
+        $state = !$state;
+        $binds = array(
+            ':status' => [intval($state), SQLITE3_INTEGER],
+            ':id' => [$id, SQLITE3_INTEGER]
+        );
+        $stmt = $db->prepare('UPDATE todo SET ' . $status . ' = :status WHERE id=:id;');
+
+        foreach ($binds as $key => $value) {
+            $stmt->bindValue($key, $value[0], $value[1]);
+        }
+
+        $ret = $stmt->execute();
+        return is_object($ret);
+    }
+    return false;
+}
 //
 //    if (is_object($result)) {
 //        $row = $result->fetchArray();
