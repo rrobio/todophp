dffa5b4b811737c871cc3eb18d9a0532a61c9fc4
rrobio <orlandinirobi@gmail.com>
2023-10-18 12:00:36 +0200

    refactor out helper functions from index.php into a todo controller

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 use internal\Router;
 use JetBrains\PhpStorm\NoReturn;
-use model\Todo;
 
 spl_autoload_register(function ($class) {
     $file = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
@@ -13,63 +12,8 @@ const BASE_DIR = __DIR__;
 $router = new Router();
 $router->serve();
 
-
 #[NoReturn] function dd($array, $name = 'var'): never
 {
     highlight_string("<?php\n\$$name =\n" . var_export($array, true) . ";\n?>");
     die();
-}
-
-function insertTodo(Todo $todo, SQLite3 $db): bool
-{
-    $binds = array(
-        ':text' => [$todo->text, SQLITE3_TEXT],
-        ':done' => [$todo->done, SQLITE3_INTEGER],
-        ':skip' => [$todo->skip, SQLITE3_INTEGER]
-    );
-
-    $stmt = $db->prepare('INSERT INTO todo (id, text, done, skip) VALUES (null, :text, :done, :skip);');
-
-    foreach ($binds as $key => $value) {
-        $stmt->bindValue($key, $value[0], $value[1]);
-    }
-
-    $ret = $stmt->execute();
-    return is_object($ret);
-}
-
-//
-function removeTodo(int $id, SQLite3 $db): bool
-{
-    $stmt = $db->prepare('DELETE FROM todo WHERE id=:id;');
-    $stmt->bindValue(':id', $id, SQLITE3_INTEGER);
-
-    $result = $stmt->execute();
-    return is_object($result);
-}
-
-function toggleStatus(int $id, string $status, SQLite3 $db): bool
-{
-    $stmt = $db->prepare('SELECT * FROM todo WHERE id=:id;');
-    $stmt->bindValue(':id', $id, SQLITE3_INTEGER);
-    $result = $stmt->execute();
-
-    if (is_object($result)) {
-        $row = $result->fetchArray();
-        $state = (bool)$row[$status];
-        $state = !$state;
-        $binds = array(
-            ':status' => [intval($state), SQLITE3_INTEGER],
-            ':id' => [$id, SQLITE3_INTEGER]
-        );
-        $stmt = $db->prepare('UPDATE todo SET ' . $status . ' = :status WHERE id=:id;');
-
-        foreach ($binds as $key => $value) {
-            $stmt->bindValue($key, $value[0], $value[1]);
-        }
-
-        $ret = $stmt->execute();
-        return is_object($ret);
-    }
-    return false;
 }
--- routes/web.php
+++ routes/web.php
@@ -2,33 +2,31 @@
 
 use controllers\Index;
 use internal\Router;
-use model\Todo;
-
 
 Router::get('/', Index::class);
 //Router::get('/todos', 'hello todos');
 
 Router::get('/toggleSkipTodo?{id}', function (?string $id) {
     $db = new SQLite3('todo.db');
-    toggleStatus((int)$id, 'skip', $db);
+    \controllers\Todo::toggleStatus((int)$id, 'skip', $db);
     header('Location: /');
 });
 
 Router::get('/toggleDoneTodo?{id}', function (?string $id) {
     $db = new SQLite3('todo.db');
-    toggleStatus((int)$id, 'done', $db);
+    \controllers\Todo::toggleStatus((int)$id, 'done', $db);
     header('Location: /');
 });
 
 Router::get('/createTodo?{text}', function (?string $text) {
     $db = new SQLite3('todo.db');
-    $ret = insertTodo(new Todo($text, false, false), $db);
+    $ret = \controllers\Todo::insertTodo($text ?? 'empty', $db);
     header('Location: /');
 });
 
 Router::get('/deleteTodo?{id}', function (?string $id) {
     $db = new SQLite3('todo.db');
-    $ret = removeTodo($id, $db);
+    $ret = \controllers\Todo::removeTodo($id, $db);
     header('Location: /');
 });
 
